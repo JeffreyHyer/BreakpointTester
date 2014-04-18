de5ade29e4433174411b8ee242c4aa521864a035
JeffreyHyer <jeffh@gogreenentinc.com>
2014-04-17 23:10:29 -0600

    v1.0.4 - Added Promos and Screenshots for Chrome Webstore, moved HISTORY to README.

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,50 @@
-var service = analytics.getService('BreakpointTester');
-var tracker = service.getTracker('UA-12477767-4');
+var service = analytics.getService('BreakpointTester'),
+	tracker = service.getTracker('UA-12477767-4'),
 
-tracker.sendAppView('Popup');
+	history = [],
+	preferences = {
+		prefer: 	'left',
+		height: 	screen.availHeight,
+		top: 		0,
+		left: 		0,
+		right: 		0
+	};
+	
+	tracker.sendAppView('Popup');
 
 $(document).on('ready', function() {
+	chrome.storage.sync.get('history', function(obj) {
+		if (obj.history) {
+			history = obj.history;
+
+			updateHistory();
+		}
+	});
+
+	chrome.windows.getCurrent(null, function(window) {
+		preferences.height = ((window.state == "maximized") ? parseInt(screen.availHeight) : parseInt(window.height));
+		preferences.top = ((window.state == "maximized") ? 0 : parseInt(window.top));
+
+		if (window.state == "maximized") {
+			preferences.prefer = 'left';
+			preferences.left = 0;
+			preferences.top = 0;
+		}
+		else if (parseInt(window.left) == 0) {
+			preferences.prefer = 'left';
+		}
+		else if ((parseInt(window.left) % (screen.availWidth)) == 0) {
+			preferences.prefer = 'left';
+			preferences.left = parseInt(window.left);
+		}
+		else if ((Math.abs(parseInt(window.left)) - parseInt(window.width)) == 0) {
+			preferences.prefer = 'right';
+			preferences.right = (parseInt(window.left) + parseInt(window.width));
+		}
+
+		updateSlider(window.width);
+	});
+
 	$('#version span').text("v" + chrome.runtime.getManifest().version);
 
 	$('#customBreakpoint').noUiSlider({
@@ -20,17 +61,13 @@ $(document).on('ready', function() {
 	$('#customBreakpoint').on('set', updateSliderText);
 	$('#customBreakpoint').on('slide', updateSliderText);
 
-	chrome.windows.getCurrent(null, function(window) {
-		updateSlider(window.width);
-	});
-
 	$(".preset").on('click', function() {
 		var width = $(this).attr('data-width');
 			width = (parseInt(width) + 16);
 
 		tracker.sendEvent('Preset', ((width - 16) + 'px'));
 
-		updateWindow({ width: parseInt(width), height: parseInt(screen.availHeight), top: 0, left: 0 });
+		updateWindow({ width: parseInt(width) });
 	});
 
 	$('#customSet').on('click', function() {
@@ -38,7 +75,7 @@ $(document).on('ready', function() {
 
 		tracker.sendEvent('Custom', (width - 16) + 'px');
 
-		updateWindow({ width: width, height: screen.availHeight, top: 0, left: 0 });
+		updateWindow({ width: parseInt(width) });
 	})
 });
 
@@ -51,15 +88,50 @@ function updateSliderText() {
 }
 
 function updateWindow(options) {
+	if (preferences.prefer == 'left') {
+		options.left = preferences.left;
+	}
+	else if (preferences.prefer == 'right') {
+		options.left = (preferences.right - options.width);
+	}
+
+	options.top = preferences.top;
+
+	options.height = preferences.height;
+
 	chrome.windows.getCurrent(null, function(window) {
 	     chrome.windows.update(
 	     	window.id,
 	     	options,
 	     	function(response) {}
 	     );
-
-	     updateSlider(options.width);
 	});
 
-	tracker.sendEvent('Resize', ((parseInt(options.width) - 16) + 'px'));
+	// Push on to the front of the array
+	history.unshift(parseInt(options.width) - 16);
+
+	// Trim the length of the array so it's always <= 5
+	history = history.filter(function(value, index) {
+		return (index < 5);
+	});
+
+	chrome.storage.sync.set({ history: history });
+
+	updateHistory();
+}
+
+function updateHistory() {
+	$ul = $("#left ul");
+	$ul.empty();
+
+	history.forEach(function(value, index) {
+		$ul.append('<li>' + value + 'px</li>');
+	});
+
+	$("#left ul li").off("click").on("click", function(event) {
+		var width = parseInt($(this).text().replace('px', ''));
+		tracker.sendEvent('History', width + 'px');
+
+		updateWindow({ width: (width + 16) });
+	});
 }
